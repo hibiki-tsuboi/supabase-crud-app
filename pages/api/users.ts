304b90adcef3aa070a8ddfba801d0a8970212419
Hibiki Tsuboi <hibiki.tsuboi@gmail.com>
2025-06-04 10:04:26 +0900

    wip

--- pages/api/users.ts
+++ pages/api/users.ts
@@ -2,13 +2,16 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import { supabase } from '../../lib/supabaseClient'
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  console.log('[handler]start')
   if (req.method === 'GET') {
+    console.log('[handler]GET')
     const { data, error } = await supabase.from('users').select('*')
     if (error) return res.status(500).json({ error: error.message })
     return res.status(200).json(data)
   }
 
   if (req.method === 'POST') {
+    console.log('[handler]POST')
     const { name, email } = req.body
     const { data, error } = await supabase.from('users').insert([{ name, email }])
     if (error) return res.status(500).json({ error: error.message })
